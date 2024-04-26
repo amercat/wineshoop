aeb783a635e5f69d7570eba5e1d7d4055df02c21
amercat25 <tbaptiste25@outlook.com>
2024-04-26 09:05:22 +0200

    ADd icon cart and shopping cart wrapper

--- app/Views/pages/contact.php
+++ app/Views/pages/contact.php
@@ -137,7 +137,9 @@
                                 <div class="w-grid__item w-grid__item--middle list__item-element_1FX" data-column-size="6">
                                     <div class="w-grid__item-inner w-grid__item-panel w-text-left">
                                         <p class="text_1k0 ui-subtitle w-subtitle ui-subtitle--light" data-component="text">
-                                            <span class="w-text-block" style="display: block;"><label class="w-text-content text-decoration-underline">Entrez votre commentaire</label></span>
+                                            <span class="w-text-block" style="display: block;">
+                                                <label class="w-text-content text-decoration-underline">Entrez votre commentaire</label>
+                                            </span>
                                         </p>
                                     </div>
                                 </div>
@@ -149,24 +151,58 @@
                                                     <span style="color: rgb(137, 128, 130);">
                                                          <span class="w-text-block" style="display: block;">
 
-
-                                                             <input type="texte" id="60116a4148c6e50022bc0276-contactForm_comment" class="w-form__input ui-input ui-input--size-md"
-                                                                    placeholder="Enter votre commentaire" required="" style="border-color: rgb(242, 233, 233);
-                                                                background-color: rgba(73, 73, 73, 0.24); --w-get-input-background-color: rgba(73, 73, 73, 0.24);
-                                                                   background-origin: padding-box, content-box;" value="" maxlength="2000"
-                                                              <div data-mdb-input-init class="form-outline w-100">
-                <textarea class="form-control w-form__input ui-input ui-input--size-md" id="textAreaExample" rows="4"
-                          style="background: #fff;"></textarea>
-                <label class="form-label" for="textAreaExample">Message</label>
-              </div>>
+                                                             <textarea type="texte" id="60116a4148c6e50022bc0276-contactForm_comment" class="w-form__input ui-input ui-input--size-md"
+                                                                       placeholder="Enter votre commentaire" required="" style="border-color: rgb(242, 233, 233);
+                                                                        background-color: rgba(73, 73, 73, 0.24); --w-get-input-background-color: rgba(73, 73, 73, 0.24);
+                                                                         background-origin: padding-box, content-box;" maxlength="2000"
+                                                                       data-mdb-input-init></textarea>
 
 
-                </li>
+                                                             <div class="w-section__content">
+                            <div class="w-grid w-grid--gap list--gap_1ww list_ID4">
+                                <ul class="w-grid__inner">
+                                    <li class="w-grid__item list__item_2m3">
+                                        <div class="w-grid__item-inner w-grid__item-panel w-088754deb0dd8820b733e12d9894218e">
+                                            <div class="w-grid w-grid--row w-grid--gap" data-columns-count="4">
+                                                <div class="w-grid__inner list__inner-element_hqn">
+                                                    <div class="w-grid__item w-grid__item--middle list__item-elemet_1FX" data-column-size="4">
+                                                        <div class="w-grid__item-inner w-grid__item-panel w-text-left">
+                                                            <div class="w-picture-wrapper media-wrapper_3VN">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+<button class="w-form-button w-form__button ui-button ui-button--primary-dark ui-button--size-md w-form-button--font-weight-bold">
+                                 <span class="w-form-button__border ui-button__border"></span>
+                                     <span class=w-form-button_content">
+                                         <span class="w-form-button__inner">
+                                             <span class="w-form-button__text">Submit
+                                             </span>
+                                         </span>
+                                     </span>
+                                    </button>
 
-            </ul>
-        </div>
-    </div>
+
+
+
+                                </ul>
+
+                            </div>
+
+
+                                    </div>
+                                </div>
 
 </div>
 </div>
+
+
+
+
+
 </body>
